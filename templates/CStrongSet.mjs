73ce0cdd8190e369de4400103c8bf777dc378c76
Alexander J. Vincent <ajvincent@gmail.com>
2021-04-10 23:38:09 -0700

    Add stubs for CStrongSet, CStrongMapOfSets.

---
+++ templates/CStrongSet.mjs
@@ -0,0 +1,44 @@
+import KeyHasher from "composite-collection/KeyHasher";
+
+export default class __className__ {
+  constructor() {
+    this.__root__ = new Set;
+  }
+
+  get size() {
+
+  }
+
+  add(__argList__) {
+
+  }
+
+  clear() {
+
+  }
+
+  delete(__argList__) {
+
+  }
+
+  forEach() {
+
+  }
+
+  has(__argList__) {
+
+  }
+
+  values() {
+
+  }
+}
+__className__[Symbol.iterator] = function() {
+  return this.values();
+}
+Reflect.defineProperty(__className__, Symbol.toStringTag, {
+  value: "__className__",
+  writable: false,
+  enumerable: false,
+  configurable: true
+});
