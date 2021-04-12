5796e9b8fce552bb9080119de58263719da3462c
Alexander J. Vincent <ajvincent@gmail.com>
2021-04-12 11:17:35 -0700

    templates/Weak/Set.mjs example code.

--- templates/Weak/Set.mjs
+++ templates/Weak/Set.mjs
@@ -1,17 +1,50 @@
+import WeakKeyComposer from "composite-collection/WeakKey-WeakRef";
+
 export default class __className__ {
-  constructor() {
-    this.__root__ = new WeakSet;
+  constructor(__weakArgNames__, __strongArgNames__) {
+    /** @type {WeakKeyComposer} */
+    this.__keyComposer__ = new WeakKeyComposer(__weakArgNames__, __strongArgNames__);
+
+    /**
+     * @type {WeakMap<WeakKey, Set<void> | object>}
+     */
+    this.__weakKeyToStrongKeys__ = new WeakMap;
   }
 
   add(__argList__) {
+    const key = this.__keyComposer__.getKey([__weakArgList__], [__strongArgList__]);
+    if (!key)
+      return null;
 
+    if (!this.__weakKeyToStrongKeys__.has(key))
+      this.__weakKeyToStrongKeys__.set(
+        key,
+        this.__strongArgNames__.length == 0 ? {} : new Set([__strongArgList__])
+      );
+
+    return this;
   }
 
   delete(__argList__) {
+    if (!this.has(__argList__))
+      return false;
 
+    const key = this.__keyComposer__.getKey([__weakArgList__], [__strongArgList__]);
+    if (!key)
+      return false;
+
+    const rv = this.__weakKeyToStrongKeys__.delete(key);
+    if (rv)
+      this.__keyComposer__.deleteKey([__weakArgList__], [__strongArgList__])
+
+    return rv;
   }
 
   has(__argList__) {
+    if (!this.__keyComposer__.hasKey([__weakArgList__], [__strongArgList__]))
+      return false;
 
+    const key = this.__keyComposer__.getKey([__weakArgList__], [__strongArgList__]);
+    return this.__weakKeyToStrongKeys__.has(key);
   }
 }
