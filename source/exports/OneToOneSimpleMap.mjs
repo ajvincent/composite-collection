6ee31f3bc6d0f4bd7674e902f1b2ecb1ba4175a7
Alexander J. Vincent <ajvincent@gmail.com>
2022-01-05 00:34:46 -0800

    #58 OneToOneMap, package exports.

--- source/exports/OneToOneSimpleMap.mjs
+++ source/exports/OneToOneSimpleMap.mjs
@@ -1,18 +1,7 @@
 import CollectionConfiguration from "composite-collection/Configuration";
 
 const OneToOneSimpleMapConfig = new CollectionConfiguration("OneToOneSimpleMap", "OneToOne");
-await OneToOneSimpleMapConfig.configureOneToOne("WeakMap", "key", {
-  // true means Object(value) === value
-  valuesMustBeObjects: true,
-
-  // true means hold all values as keys in #strongValueToInternalKeyMap
-  holdValuesStrongly: false,
-
-  /* This doesn't apply in the WeakMap case.
-  // include if you want to import the module instead of having it inline.
-  pathToBaseModule: "./WeakStrongMap.mjs",
-  */
-});
+await OneToOneSimpleMapConfig.configureOneToOne("WeakMap", "key");
 OneToOneSimpleMapConfig.lock();
 
 export default OneToOneSimpleMapConfig;
