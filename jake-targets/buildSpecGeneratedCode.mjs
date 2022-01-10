05f9930ea154c1fb53832e7782ca4a84322473ce
Alexander J. Vincent <ajvincent@gmail.com>
2022-01-09 19:41:56 -0800

    #60, decouple support/build from configurations in specs.

--- jake-targets/buildSpecGeneratedCode.mjs
+++ jake-targets/buildSpecGeneratedCode.mjs
@@ -5,6 +5,7 @@ import url from "url";
 import runDriver from "./runDriver.mjs";
 import {
   PromiseAllSequence,
+  PromiseAllParallel,
 } from "#support/generateCollectionTools.mjs";
 
 import readDirsDeep from "#source/utilities/readDirsDeep.mjs";
@@ -17,13 +18,34 @@ const specRoot = path.join(process.cwd(), "spec");
  * @param {fs.DirEnt} dirEntry The directory entry.
  * @returns {string}  The full path to the directory, or "" if there is no configurations subdirectory.
  */
+async function hasSupportBuild(dirEntry) {
+  if (!dirEntry.isDirectory())
+    return "";
+  const target = path.join(specRoot, dirEntry.name);
+  const fullPath = path.join(target, "support/build.mjs");
+  try {
+    const stats = await fs.stat(fullPath);
+    return stats.isFile() ? target : "";
+  }
+  catch (ex) {
+    return "";
+  }
+}
+
+/**
+ * Determine if a spec directory has a configurations subdirectory.
+ *
+ * @param {fs.DirEnt} dirEntry The directory entry.
+ * @returns {string}  The full path to the directory, or "" if there is no configurations subdirectory.
+ */
 async function hasConfigurationSubdirectory(dirEntry) {
   if (!dirEntry.isDirectory())
     return "";
-  const fullPath = path.join(specRoot, dirEntry.name, "configurations");
+  const target = path.join(specRoot, dirEntry.name);
+  const fullPath = path.join(target, "configurations");
   try {
     const stats = await fs.stat(fullPath);
-    return stats.isDirectory() ? path.dirname(fullPath) : "";
+    return stats.isDirectory() ? target : "";
   }
   catch (ex) {
     return "";
@@ -43,70 +65,63 @@ async function invokeSpecBuildModule(targetFile) {
   await targetModule();
 }
 
-const specDirs = [];
-{
-  const children = await fs.readdir(
-    path.join(process.cwd(), "spec"),
-    {encoding: "utf-8", "withFileTypes": true}
-  );
+/**
+ * @param {Function} filter The filter function.
+ * @param {Function} action The action to take.
+ */
+async function iterateOverSpecDirs(filter, action) {
+  const directories = (await PromiseAllParallel(specDirs, filter)).filter(Boolean);
 
-  const matches = await Promise.all(children.map(hasConfigurationSubdirectory));
-  specDirs.push(...matches.filter(Boolean));
+  // Ensure the generated directory exists.
+  await PromiseAllSequence(directories, async specDir => {
+    const generatedDir = path.join(specDir, "generated");
+    let stats;
+    try {
+      stats = await fs.stat(generatedDir);
+    }
+    catch (ex) {
+      // do nothing
+    }
+    if (!stats?.isDirectory())
+      await fs.mkdir(generatedDir, { recursive: true });
+  });
+
+  await PromiseAllSequence(directories, action);
 }
 
-await PromiseAllSequence(specDirs, async specDir => {
-  const generatedDir = path.join(specDir, "generated");
-  console.log("Starting " + specDir.replace(process.cwd(), ""));
-  let stats;
+const specDirs = await fs.readdir(
+  path.join(process.cwd(), "spec"),
+  {encoding: "utf-8", "withFileTypes": true}
+);
 
-  try {
-    stats = await fs.stat(generatedDir);
+await iterateOverSpecDirs(
+  hasSupportBuild,
+  async specDir => {
+    const buildModulePath = path.join(specDir, "support/build.mjs");
+    let stats;
+    try {
+      stats = await fs.stat(buildModulePath);
+    }
+    catch (ex) {
+      // do nothing
+    }
+    if (stats?.isFile()) {
+      console.log("Found support/build.mjs file");
+      await invokeSpecBuildModule(buildModulePath);
+    }
   }
-  catch (ex) {
-    // do nothing
-  }
-  if (!stats?.isDirectory())
-    await fs.mkdir(generatedDir);
+);
 
-  const buildModulePath = path.join(specDir, "support/build.mjs");
-  try {
-    stats = await fs.stat(buildModulePath);
+await iterateOverSpecDirs(
+  hasConfigurationSubdirectory,
+  async specDir => {
+    const generatedDir = path.join(specDir, "generated");
+    console.log("Invoking Driver for " + specDir.replace(process.cwd(), ""));
+    const configurationsDir = path.join(specDir, "configurations");
+    await fs.mkdir(configurationsDir, { recursive: true });
+    await runDriver(configurationsDir, generatedDir);
   }
-  catch (ex) {
-    // do nothing
-  }
-  if (stats?.isFile()) {
-    console.log("Found support/build.mjs file");
-    await invokeSpecBuildModule(buildModulePath);
-  }
-
-  console.log("Starting Driver.mjs to generate collection files");
-  const configurationsDir = path.join(specDir, "configurations");
-  await runDriver(configurationsDir, generatedDir);
-
-  console.log("Completed building " + specDir.replace(process.cwd(), ""));
-});
-
-// source/exports -> spec/_04_exports/generated
-{
-  const specDir = path.join(process.cwd(), "spec/_04_exports");
-  console.log("Starting /spec/_04_exports");
-
-  const buildModulePath = path.join(specDir, "support/build.mjs");
-  let stats;
-  try {
-    stats = await fs.stat(buildModulePath);
-  }
-  catch (ex) {
-    // do nothing
-  }
-  if (stats?.isFile()) {
-    console.log("Found support/build.mjs file");
-    await invokeSpecBuildModule(buildModulePath);
-  }
-
-  console.log("Completed building /spec/_04_exports");
-}
+);
 
 console.log("Started verifying generated modules");
 {
