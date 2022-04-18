b4df0a48ae97ef8cdc6e16844add90f3e35169ab
Alexander J. Vincent <ajvincent@gmail.com>
2022-04-17 23:21:08 -0700

    #85, remove unused CompletionPromise copy.

--- source/utilities/PromiseTypes.mjs
+++ source/utilities/PromiseTypes.mjs
@@ -49,28 +49,4 @@ export async function PromiseAllSequence(elementArray, callback) {
 export async function PromiseAllParallel(elementArray, callback) {
     return Promise.all(elementArray.map(element => callback(element)));
 }
-export class CompletionPromise {
-    #abortException = null;
-    #completionPromise;
-    constructor(startPromise, resolver) {
-        this.#completionPromise = startPromise.then(resolver).catch(abortException => this.abort(abortException));
-    }
-    get completionPromise() {
-        return this.#completionPromise;
-    }
-    /**
-     * Abort processing and forward the exception.
-     *
-     * @param {Error} exception The rejection value.
-     * @throws
-     */
-    async abort(exception) {
-        this.#abortException = exception;
-        throw exception;
-    }
-    /** @type {Error | null} */
-    get abortException() {
-        return this.#abortException;
-    }
-}
 //# sourceMappingURL=PromiseTypes.mjs.map
