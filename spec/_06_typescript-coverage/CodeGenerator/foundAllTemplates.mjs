8a12a5030278575370b99dcf33e1c302c9dac452
Alexander J. Vincent <ajvincent@gmail.com>
2022-04-28 20:37:18 -0700

    Some diagnostic logging to go with the TypeScript template errors.

--- spec/_06_typescript-coverage/CodeGenerator/foundAllTemplates.mjs
+++ spec/_06_typescript-coverage/CodeGenerator/foundAllTemplates.mjs
@@ -8,6 +8,31 @@ import { PromiseAllParallel } from "#source/utilities/PromiseTypes.mjs";
 const specDir = url.fileURLToPath(new URL("..", import.meta.url));
 const projectRoot = url.fileURLToPath(new URL("../../..", import.meta.url));
 
+import StrongMapOfStrongSets from "../generated/StrongMapOfStrongSets.mjs";
+
+class TemplateSet extends StrongMapOfStrongSets {
+  templateKeys = new Set;
+
+  add(template, relativePath) {
+    super.add(template, relativePath);
+    this.templateKeys.add(template);
+  }
+
+  markDown() {
+    const orderTemplateKeys = Array.from(this.templateKeys.values());
+    orderTemplateKeys.sort();
+
+    return orderTemplateKeys.map(key => this.#markDownSet(key)).join("\n");
+  }
+
+  #markDownSet(template) {
+    const keys = Array.from(this.valuesSet(template));
+    keys.sort();
+
+    return `- ${template}\n` + keys.map(item => `  - ${item[1]}`).join("\n");
+  }
+}
+
 it("Code coverage of all templates", async () => {
   let templates;
   {
@@ -21,6 +46,8 @@ it("Code coverage of all templates", async () => {
     }));
   }
 
+  const tMap = new TemplateSet;
+
   {
     const generatedPath = path.join(specDir, "generated");
     let files = await fs.readdir(generatedPath);
@@ -30,9 +57,17 @@ it("Code coverage of all templates", async () => {
       const contents = await fs.readFile(pathToFile, { encoding: "utf-8" });
       const foundTemplate = /Template: ([^\n]+)\n/.exec(contents)[1];
       templates.delete(foundTemplate);
+
+      tMap.add(foundTemplate, pathToFile.replace(generatedPath + "/", ""));
     });
   }
 
   if (templates.size)
     fail(Array.from(templates));
+
+  await fs.writeFile(
+    path.join(specDir, "template-collections.md"),
+    `# Templates\n\n` + tMap.markDown() + "\n",
+    { encoding: "utf-8" }
+  );
 });
